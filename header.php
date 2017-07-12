41ee27d9d961748d3e58bb543acc3d95e4bed35c
Zachary Smith <info@zachis.it>
2017-07-12 07:29:39 -0400

    adding title to header

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11">
-
+    <title><?php if ( !is_front_page() ) { wp_title( '|', true, 'right' ); } bloginfo( 'name' ); ?></title>
     <?php wp_head(); ?>
 </head>
 <body>
