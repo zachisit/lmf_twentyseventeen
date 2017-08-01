77110e37a1bcacfafaf39ceb53d48ce517fb7310
Zachary Smith <info@zachis.it>
2017-08-01 13:57:41 -0400

    proof of concept, but really bad way to code: print order details: showing order details with thickbox and fontawesome

--- woo/woo_specifics.php
+++ woo/woo_specifics.php
@@ -30,12 +30,12 @@ function lmf_remove_wc_breadcrumbs() {
 /*************************************************************
  * Print Order Detail
  * Backend of Orders Table
- ************************************************************
+ *************************************************************/
 add_action( 'init', 'lmf_remove_wc_breadcrumbs' );
 
 add_filter( 'manage_shop_order_posts_columns', 'set_custom_edit_post_columns',99,1 );
 function set_custom_edit_post_columns($columns) {
-    $columns['custom-columns'] = __( 'Print Order Details', 'print_order_details' );
+    $columns['custom-columns'] = __( 'Print Order', 'print_order_details' );
     return $columns;
 }
 add_action( 'manage_shop_order_posts_custom_column' , 'custom_cpost_column', 99, 2 );
@@ -43,12 +43,126 @@ function custom_cpost_column( $column, $post_id ) {
     switch ( $column ) {
 
         case 'custom-columns'://new-title=your column slug :
-            //echo 'custom columns value' ;
-            $order = wc_get_order( $post_id );
-            $order_data = $order->get_data(); // The Order data
-            $order_payment_method = $order_data['payment_method'];
-            echo $order_payment_method;
-            break;
+            add_thickbox();
+        ?>
+            <style>
+                .fa {
+                    font-size:1.8em !important;
+                    color:#999;
+                }
+            </style>
+            <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+
+            <a href="#TB_inline?width=600&height=550&inlineId=order_detail_view" class="thickbox"><i class="fa fa-info-circle" aria-hidden="true"></i></a>
+<div id="order_detail_view" style="display:none;">
+    <p>
+        <?php
+        //echo 'custom columns value' ;
+        $order = wc_get_order( $post_id );
+        $order_data = $order->get_data(); //the Order data
+
+        $data_order_created = [
+            'd_c_' => $order_date_created = $order_data['date_created']->date('Y-m-d H:i:s'),
+        'd_c_m' => $order_date_modified = $order_data['date_modified']->date('Y-m-d H:i:s'),
+        'd_c_i' => $order_id = $order_data['id'],
+        'd_c_pid' => $order_parent_id = $order_data['parent_id'],
+        'd_c_s' => $order_status = $order_data['status'],
+        'd_c_c' => $order_currency = $order_data['currency'],
+        'd_c_v' => $order_version = $order_data['version'],
+        'd_c_pm' => $order_payment_method = $order_data['payment_method'],
+        'd_c_pmt' => $order_payment_method_title = $order_data['payment_method_title'],
+        ];
+
+        $data_shipping = [
+            's_s' => $order_shipping_first_name = $order_data['shipping']['first_name'],
+        's_ln' => $order_shipping_last_name = $order_data['shipping']['last_name'],
+        's_c' => $order_shipping_company = $order_data['shipping']['company'],
+        's_aone' => $order_shipping_address_1 = $order_data['shipping']['address_1'],
+        's_atwo' => $order_shipping_address_2 = $order_data['shipping']['address_2'],
+        's_c' => $order_shipping_city = $order_data['shipping']['city'],
+        's_st' => $order_shipping_state = $order_data['shipping']['state'],
+        's_z' => $order_shipping_postcode = $order_data['shipping']['postcode'],
+        's_ccc' => $order_shipping_country = $order_data['shipping']['country'],
+        ];
+
+        $data_billing = [
+            'b_fname' => $order_billing_first_name = $order_data['billing']['first_name'],
+            'b_lname' => $order_billing_last_name = $order_data['billing']['last_name'],
+        ];
+
+        //$order_payment_method = $order_data['payment_method'];
+        foreach ($data_order_created as $key => $value){
+            echo $value."\n";
+        }
+        foreach ($data_shipping as $key => $value){
+            echo $value."\n";
+        }
+        foreach ($data_billing as $key => $value){
+            echo $value."\n";
+        }
+
+        /*-------------
+        order details
+        ------------*/
+        // Get an instance of the WC_Order object
+        $order = wc_get_order( $order_id );
+
+        // Iterating through each WC_Order_Item objects
+        foreach( $order-> get_items() as $item_key => $item_values ):
+
+            ## Using WC_Order_Item methods ##
+
+            // Item ID is directly accessible from the $item_key in the foreach loop or
+            $item_id = $item_values->get_id();
+
+            $item_name = $item_values->get_name(); // Name of the product
+            $item_type = $item_values->get_type(); // Type of the order item ("line_item")
+
+            ## Access Order Items data properties (in an array of values) ##
+            $item_data = $item_values->get_data();
+
+    $product_name = $item_data['name'];
+    $product_id = $item_data['product_id'];
+    $variation_id = $item_data['variation_id'];
+    $quantity = $item_data['quantity'];
+    $tax_class = $item_data['tax_class'];
+    $line_subtotal = $item_data['subtotal'];
+    $line_subtotal_tax = $item_data['subtotal_tax'];
+    $line_total = $item_data['total'];
+    $line_total_tax = $item_data['total_tax'];
+
+    echo '<p></p>';
+    echo '<strong>product name:</strong>'. $product_name . '<br /><br />';
+    echo '<strong>product id:</strong>'. $product_id . '<br /><br />';
+    echo '<strong>product qty:</strong>'. $quantity . '<br /><br />';
+    echo '<strong>product tax:</strong>'. $tax_class . '<br /><br />';
+    echo '<strong>product sub:</strong>'. $line_subtotal . '<br /><br />';
+    echo '<strong>product total:</strong>'. $line_subtotal_tax . '<br /><br />';
+    echo '<strong>product total:</strong>'. $line_total . '<br /><br />';
+    echo '<strong>product tax:</strong>'. $line_total_tax . '<br /><br />';
+
+        endforeach;
+        ?>
+        <script>
+            jQuery('.printMe').click(function(){
+                window.print();
+            });//@todo turn into button not alink
+        </script>
+    <style>
+        #print {
+            display:block;
+            margin:2em 0;
+            float:left;
+        }
+        </style>
+        <div id="print">
+        <a href="#" class=".printMe"><i class="fa fa-print" aria-hidden="true"></i> print this</a>
+    </div>
+    </p>
+</div>
+<?php
+
+
+
     }
 }
- */
