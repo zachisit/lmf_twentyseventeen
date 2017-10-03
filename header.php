d1c23d1a161b317466b43b9159b63e5fb453d369
Zachary Smith <info@zachis.it>
2017-10-03 07:38:33 -0400

    updated announcemnts show content if has featured image; updated header menu placement to be margin auto in middle; updatd content layout

--- header.php
+++ header.php
@@ -46,51 +46,55 @@
         <p><a href="<?php echo get_home_url(); ?>/contact" title="Directions">Directions</a></p>
     </div>
 <header>
-    <div id="logo">
-        <a href="<?php echo get_home_url(); ?>" title="Home"><img src="<?php echo get_template_directory_uri(); ?>/images/logo-twentyseventeen.png" alt="<?php echo get_bloginfo( 'name' ); ?> - Home" /></a>
-    </div>
-    <div id="right">
-        <div id="top">
-            <div id="one">
-                <ul>
-                    <li><a href="https://www.facebook.com/LouisianaMusicFactory" title="<?php echo get_bloginfo( 'name' ); ?> Facebook" target="_blank"><i class="fa fa-facebook-official" aria-hidden="true"></i>
-                        </a></li>
-                    <li><a href="https://twitter.com/LMFnola" title="<?php echo get_bloginfo( 'name' ); ?> Twitter" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i>
-                        </a></li>
-                    <li><a href="http://www.youtube.com/user/LAMUSICFACTORY" title="<?php echo get_bloginfo( 'name' ); ?> YouTube" target="_blank"><i class="fa fa-youtube" aria-hidden="true"></i>
-                        </a></li>
-                    <li><a href="https://www.instagram.com/louisianamusicfactory/" title="<?php echo get_bloginfo( 'name' ); ?> Instagram" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i>
-                        </a></li>
-                </ul>
-            </div>
-            <div id="two">
-                <div id="phone"><a href="tel:+15045861094">504-586-1094</a></div>
-            </div>
-            <div id="three">
-                <div id="contact"><a href="<?php echo get_home_url(); ?>/contact#contact" title="Contact <?php echo get_bloginfo( 'name' ); ?>">Contact Us</a></div>
-            </div>
-            <div id="four">
-                <div id="login">
-                    <?php
-                    if ( is_user_logged_in() ) {
-                        $current_user = wp_get_current_user();
+    <div id="top">
+        <div id="logo">
+            <a href="<?php echo get_home_url(); ?>" title="Home"><img src="<?php echo get_template_directory_uri(); ?>/images/logo-twentyseventeen.png" alt="<?php echo get_bloginfo( 'name' ); ?> - Home" /></a>
+        </div>
+        <div id="right">
+            <div id="top">
+                <div id="one">
+                    <ul>
+                        <li><a href="https://www.facebook.com/LouisianaMusicFactory" title="<?php echo get_bloginfo( 'name' ); ?> Facebook" target="_blank"><i class="fa fa-facebook-official" aria-hidden="true"></i>
+                            </a></li>
+                        <li><a href="https://twitter.com/LMFnola" title="<?php echo get_bloginfo( 'name' ); ?> Twitter" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i>
+                            </a></li>
+                        <li><a href="http://www.youtube.com/user/LAMUSICFACTORY" title="<?php echo get_bloginfo( 'name' ); ?> YouTube" target="_blank"><i class="fa fa-youtube" aria-hidden="true"></i>
+                            </a></li>
+                        <li><a href="https://www.instagram.com/louisianamusicfactory/" title="<?php echo get_bloginfo( 'name' ); ?> Instagram" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i>
+                            </a></li>
+                    </ul>
+                </div>
+                <div id="two">
+                    <div id="phone"><a href="tel:+15045861094">504-586-1094</a></div>
+                </div>
+                <div id="three">
+                    <div id="contact"><a href="<?php echo get_home_url(); ?>/contact#contact" title="Contact <?php echo get_bloginfo( 'name' ); ?>">Contact Us</a></div>
+                </div>
+                <div id="four">
+                    <div id="login">
+                        <?php
+                        if ( is_user_logged_in() ) {
+                            $current_user = wp_get_current_user();
 
-                        echo 'Welcome '. $current_user->user_login .'<br />';
-                        echo '<a href="'. get_home_url() .'/my-account-2/" title="My Account">My Account</a> | <a href="'. wp_logout_url( 'http://multisiteparent.com' ) .'">Logout</a>';
-                    } else {
-                        echo'<a href="' .get_home_url() .'/my-account-2/" title="' .get_bloginfo( 'name' ) .' Customer Login">Customer Login</a>';
-                    }
-                    ?>
+                            echo 'Welcome '. $current_user->user_login .'<br />';
+                            echo '<a href="'. get_home_url() .'/my-account-2/" title="My Account">My Account</a> | <a href="'. wp_logout_url( 'http://multisiteparent.com' ) .'">Logout</a>';
+                        } else {
+                            echo'<a href="' .get_home_url() .'/my-account-2/" title="' .get_bloginfo( 'name' ) .' Customer Login">Customer Login</a>';
+                        }
+                        ?>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
     <div id="bottom">
-        <div id="search">
-            <?php dynamic_sidebar( 'header-search' );?>
-        </div>
-        <div id="menu"><a href="#" id="menu_close">X</a>
-            <?php wp_nav_menu( array( 'theme_location' => 'header_menu', 'menu_id' => 'primary-menu' ) ); ?>
+        <div id="box">
+            <div id="search">
+                <?php dynamic_sidebar( 'header-search' );?>
+            </div>
+            <div id="menu">
+                <?php wp_nav_menu( array( 'theme_location' => 'header_menu', 'menu_id' => 'primary-menu' ) ); ?>
+            </div>
         </div>
     </div>
 </header>
